2c419ff0b604192802dc252f09ba05065f34a998
Federico Summo <88909535+fedesummo@users.noreply.github.com>
2025-03-09 22:56:22 -0300

    first commit

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
-import { Footer } from "../footer";
 import { Header } from "../header";
 
 export const Layout = () => (
   <div>
     <Header />
     <Outlet />
-    {/* <Footer /> */}
   </div>
 );
